b1e193869bd4e30e1ef13a3a5e4bbdae5384f0c7
Richard <richardabarter@gmail.com>
2023-03-04 10:55:42 +0000

    login form partial

--- testingplayground/src/App.js
+++ testingplayground/src/App.js
@@ -16,6 +16,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true) 
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
 
   //before using service was this way:
   //   const hook = () => {
@@ -102,10 +105,37 @@ const App = () => {
 
   console.log('before render');
   console.log('notes are ', notesToShow);
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
